dcc5dbbc2dd98e23adff04fa75a081e8843093d4
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-11 16:50:26 +0530

    modified readme

--- Question-99.c
+++ Question-99.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main (){
-    for(int i=39;i<=100;i++){
-        printf("| %d | |[Click Here](https://github.com/Mehfila-Parkkulthil/C-/blob/main/Question-%d.c) | \n" , i,i);
+    for(int i=1;i<=100;i++){
+        printf("| %d | |[Click Here](https://github.com/Mehfila-Parkkulthil/Java--Questions/blob/main/Question%d.java) | \n" , i,i);
     }
 }

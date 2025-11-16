98fc6c1c8b96fa444aef8ad99f83daa2546440a7
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-16 16:22:27 +0530

    modified files

---
+++ Question-60.c
@@ -0,0 +1,11 @@
+//Question 60
+//Define Variables for storing a users first name ; last name and age using appropriate naming comvention and then display them
+
+#include <stdio.h>
+
+int main(){
+    char first_name[20]="Aiera";
+    char last_name[20]="Parkkulthil";
+    int age =24;
+    printf("First name:%s , Last name : %s , age : %d  ", first_name, last_name,age);
+   return 0;}

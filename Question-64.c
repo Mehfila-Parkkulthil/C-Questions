daeff1de14273f6f29e2c9f53c05815abde71c21
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-16 16:54:51 +0530

    modified files

---
+++ Question-64.c
@@ -0,0 +1,14 @@
+//Question 64
+//Create a program to calculate the product of two floating points numbers
+
+#include <stdio.h>
+int main(){
+    float a , b;
+    printf("Enter your first number : \n");
+    scanf("%f", &a);
+    printf("Enter your second number : \n");
+    scanf("%f",&b);
+    printf("So the product of your numbers are : %f", a*b);
+    return 0;
+
+}

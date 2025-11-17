8d90d0ecf21bc6a60b2d633113817be25fce2885
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-17 15:55:58 +0530

    modified files

--- Question-75.c
+++ Question-75.c
@@ -1,2 +1,18 @@
 //Question 75
-//Create a program to based on a students score categorize as "high", "moderate", "low " using ternary operator; High for scores above 80 , moderate for 50-80 ; low for below 50;
+//Create a program to based on a students score categorize as "high", "moderate", "low " using ternary operator; High for scores above 80 , moderate for 50-80 ; low for below 50;using ternary operator
+
+
+#include <stdio.h>
+int main(){
+    printf("CATEGORY OF YOUR MARKS\n");
+    int marks;
+    printf("Enter your mark:\n");
+    scanf("%d",&marks);
+
+    //Using ternary 
+    marks>80 ?  printf("High") 
+              : (marks>=50 ? printf("Moderate") //nested ternary
+                           : printf("Low"));
+
+    return 0;
+}
--- Question-76.c
+++ Question-76.c
@@ -1,2 +1,40 @@
 //Question 76
 //CReate a program to print the month of the year based on a number(1-12)input by the user.
+
+#include <stdio.h>
+int main(){
+    int month;
+    printf("Welcome to Month Guesser\n");
+    printf("Please enter the month number:\n");
+    scanf("%d",&month);
+
+    switch(month){
+        case 1: printf("Jan");
+        break;
+          case 2: printf("Feb");
+        break;
+          case 3: printf("Mar");
+        break;
+          case 4: printf("Apr");
+        break;
+          case 5: printf("May");
+        break;
+          case 6: printf("Jun");
+        break;
+          case 7: printf("Jul");
+        break;
+          case 8: printf("Aug");
+        break;
+          case 9: printf("Sept");
+        break;
+          case 10: printf("Oct");
+        break; 
+         case 11: printf("Nov");
+        break;
+          case 12: printf("Dec");
+        break;
+        default:
+        printf("Invalid month , please enter between 1-12\n");
+    }
+    return 0;
+}

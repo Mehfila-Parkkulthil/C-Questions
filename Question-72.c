cfcbe26dcd103534126c23b11f642bc86ef09c02
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-17 15:21:43 +0530

    modified files

--- Question-72.c
+++ Question-72.c
@@ -6,6 +6,27 @@ A- above 90%
 B-above 75%
 C-above 60%
 D-above 30%
-E-below 30%
+Fail-below 30%
 
 */
+
+#include <stdio.h>
+int main(){
+    printf("GRADE CALCULATOR\n");
+    printf("Enter your marks : \n");
+    int marks;
+    scanf("%d", &marks);
+    if(marks>100 || marks<0){
+        printf("Invalid marks.");
+    }else if(marks>=90 && marks <=100){
+        printf("Congrats ! A Grade");
+    }else if(marks>=75){
+        printf("Excellent ! B Grade");
+    }else if(marks>=60){
+        printf("Good Job ! C Grade");
+    }else if(marks>=30){
+        printf("Need to Improve ! D Grade");
+    }else {
+        printf("Failed !!");
+    }
+}

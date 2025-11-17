6b479d9747afa4747fbfd98c66df8bf7a1d6c6a3
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-17 15:35:49 +0530

    modified files

--- Question-73.c
+++ Question-73.c
@@ -1,2 +1,24 @@
 //Question 73
 //Create a program to find the minimum of two numbers using ternary operator
+
+#include <stdio.h>
+int main(){
+    printf("Lets find who is the bacteria \n");
+    int first , second;
+    printf("Enter your first number:\n");
+    scanf("%d",&first);
+    printf("Enter your second number:\n");
+    scanf("%d",&second);
+
+    //using ternary operator
+    first>second? printf("Your second number %d is the bacteria ",second)
+                 :printf("Your first number %d is the bacteria",first);
+
+    /*
+
+    int min =first<second?first:second;
+    printf("The bacteria is %d", min);
+
+    */
+    
+}
--- Question-74.c
+++ Question-74.c
@@ -1,2 +1,17 @@
 //Question 74
 //Create a programt to calculate the absolute value of a given integer using ternary operator
+
+#include <stdio.h>
+int main(){
+    printf("ABSOLUTE VALUE\n");
+    int n , abs_value;
+    printf("Enter your number:\n");
+    scanf("%d",&n);
+
+    //using ternary operatore
+    abs_value = n > 0 ? n : -n;
+    printf("%d is the absolute value \n", abs_value);
+    //other way
+    printf("i.e, Absolute value of %d is %d ", n , (n > 0 ? n : -n));
+
+}

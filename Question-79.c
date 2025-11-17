ad36a8f8077e0b192b00d714717c06cde68c591d
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-17 18:47:58 +0530

    modified files

--- Question-79.c
+++ Question-79.c
@@ -1,2 +1,20 @@
 //Quesiton 79
 //Create a program to calculate the sum of the digits of a number
+
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Lets calculate the sum of your digits\n");
+    printf("Enter your number :\n");
+    scanf("%d",&n);
+
+    int sum= 0;
+    int num =n;
+    
+    while(n>0){
+        sum = sum + (n%10 ); //sum = 0 + 3; //sum =0+3+2; //sum=0+3+2+1;
+        n = n/10; // n= 123/10 =12;  // n=12/10=1 ;  // n=1/10 =0 ; so loop stops
+    }
+    printf("The sum of your number %d is %d \n", num ,sum);
+    return 0;
+}

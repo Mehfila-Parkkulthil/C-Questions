a13a9dfb891fa07c7f13536ba9c5f7de4cd0bf86
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-17 16:41:26 +0530

    modified files

--- Question-77.c
+++ Question-77.c
@@ -1,2 +1,36 @@
 //Question 77
-//Create a program to create a simple calculator that uses a switch statement to perform basic arithmetic operations like addition , subtraction , multiplication and division.
+//Create a program  using switch to create a simple calculator that uses a switch statement to perform basic arithmetic operations like addition , subtraction , multiplication and division.
+
+#include <stdio.h>
+int main(){
+    printf("CALCULATOR\n");
+    float first ,second;
+    printf("Enter your number:\n");
+    scanf("%f",&first);
+    printf("Enter your second number:\n");
+    scanf("%f",&second);
+
+    char operator;
+
+    printf("Now enter which arithmetic operation you want to conduct\n");
+    printf("Enter (+ , - , * , / ) : ");
+    scanf(" %c ",&operator);
+    int invalid =0;
+    switch (operator){
+    case '+': printf("Sum is %f ", first+second);
+    break;
+    case '-': printf("Difference is %f ", first-second);
+    break;
+    case '*': printf("Product is %f", first*second);
+    break;
+    case '/': printf("Quotient is %f", first/second);
+    break;
+    default:
+    invalid =1;
+    printf("Invalid operation");
+    break;
+    }
+    if(invalid==0){
+        printf("Thank you");
+    }
+}
--- Question-78.c
+++ Question-78.c
@@ -1,2 +1,22 @@
 //Question 78
 //Create a program to calculate sum of all oddd numbers from 1 to n
+
+#include <stdio.h>
+int main(){
+    printf(" \n");
+    printf("Lets calculate sum of all odd numbers form 1 to N (excluding N)\n");
+    int N ,sum ;
+    printf("Enter your number N :  ");
+    scanf("%d",&N);
+    sum =0;
+    printf("So the numbers are :  ");
+    for(int i =1 ; i<N;i++){
+        if(i%2!=0){
+            sum+=i;
+            printf("%d  " , i);
+        }
+        
+    }
+    printf("\n");
+    printf("The sum of all odd numbers are : %d", sum);
+}

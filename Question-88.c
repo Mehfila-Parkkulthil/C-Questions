bd45e435d25fc578ff44f2400b29b3db062b730a
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-21 23:47:33 +0530

    modified readme.md file

--- Question-88.c
+++ Question-88.c
@@ -7,3 +7,16 @@ Print
 11115
 */
 
+#include <stdio.h>
+int main(){
+  int n;
+  printf("Lets print the pattern above\n");
+  for(int i =1 ; i<=5;i++){
+    printf("%d",i);
+    if(i>1){
+       for(int j=1;j<=5;j++){
+       printf("%d",j);
+    }}
+    printf("\n");
+  }
+}

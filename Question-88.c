e23c2abae391615b94274d3022e063c2b35c9888
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-21 23:56:34 +0530

    modified question 88

--- Question-88.c
+++ Question-88.c
@@ -11,8 +11,14 @@ Print
 int main(){
   int n;
   printf("Lets print the pattern above\n");
-  for(int i =1 ; i<=5;i++){
-    printf("%d",i);
-    printf("\n");
+  for(int i=1;i<=5;i++){
+    for(int j=1;j<=5;j++){
+       if(i!=j){
+       printf("%d",1);
+    }else{
+      printf("%d",j);
+    }
   }
-}
+  printf("\n");
+
+ }}
